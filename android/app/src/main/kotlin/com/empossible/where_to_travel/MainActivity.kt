484df0aa1c645eb2a342e02b458e7239849939b6
Muhammad7 <muhammad.mu.july@gmail.com>
2021-06-13 14:49:56 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/empossible/where_to_travel/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.empossible.where_to_travel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:where_to_travel/screens/home_screen.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      title: 'Where To Travel',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Color(0xFF3EBACE),
+        accentColor: Color(0xFFD8ECF1),
+        scaffoldBackgroundColor: Color(0xFFF3F5F7),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
